8386b6c225fa3c8ad96b9181cd0b83208beefff8
Junichi Hayashi <j-hayashi@seesaa.co.jp>
2018-08-28 12:51:35 +0900

    Exercise 21.4

--- chap21/Acme-NAHCNUJ-Utils/lib/Acme/NAHCNUJ/Utils.pm
+++ chap21/Acme-NAHCNUJ-Utils/lib/Acme/NAHCNUJ/Utils.pm
@@ -13,11 +13,11 @@ Acme::NAHCNUJ::Utils - Answer for exercise 21.3 of the book "Intermediate Perl"
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
